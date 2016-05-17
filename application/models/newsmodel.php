d638986c402ccee6660a1aab4d618e2f27b8ef77
Jeremiah Caballero <jeremiah.caballero.jc@gmail.com>
2016-05-17 19:24:34 +0800

    change news model

--- application/models/newsmodel.php
+++ application/models/newsmodel.php
@@ -2,23 +2,7 @@
 
 class Newsmodel	extends CI_Model
 {
-<<<<<<< HEAD
-	public function post(){
-		
-	}
 
-	public function vote(){
-		$news_id = $this->input->post('news_id');
-		$user_id = $this->input->post('user_id');
-
-		$data = array(
-			'news_id'=>$news_id,
-			'user_id'=>$user_id
-			);
-		$this->db->insert('vote',$data);
-
-	}
-=======
     public function __construct() {
         parent::__construct();
     }
@@ -32,7 +16,7 @@ class Newsmodel	extends CI_Model
         return $query->result();
     }
         
->>>>>>> dbd5069613737764caa1f0201f8d0accb004a9c3
+
 }
 
 ?>
