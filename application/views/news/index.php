c9399bc41963609c4305e1682e661bfb223fb1ca
unknown <jeremiah.caballero.jc@gmail.com>
2016-05-18 16:16:47 +0800

    made changes in index.php(news page)

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,5 +1,5 @@
 <div class="ui centered grid">
-        <div class="two column row">
+        <div class="column row">
             <div class="six wide column">
                 <div class="ui form">
                     <div class="fields">
@@ -13,29 +13,57 @@
                 </div>
             </div>
         </div>
-        <div class="two column row">
+        <div class="column row">
             <div class="six wide column">
               <div class="ui green segment">
-                    <div class="two column row">
+                <div class="ui grid">
+                    <div class="three column row">
+                        
+                            <div class="left floated column">
+                               <img src="assets/images/avatar/nan.jpg" class="ui avatar image">
+                               <label><?=$post_user?></label>
+                            </div>
+                            <div class="right floated column">
+                                    <span ><?=$post_dt?></span>
+                                    <i class="settings icon"></i>
+                            </div>
+                        
+                    </div>
+                     
+                    <div class="three column row">
                         <div class="column">
-                           <img src="assets/images/avatar/nan.jpg" class="ui avatar image">
-                           <label><?=$post_user?></label>
-                        </div>
-                        <div class=" column">
-                                <span ><?=$post_dt?></span>
-                                <i class="settings icon"></i>
+                            <p><?php echo $post_content;?></p>
                         </div>
                     </div>
-                    <div class="ui divider"></div> 
-                    <div class="two column row">
-                        <p><?php echo $post_content;?></p>
-                    </div>
-                    <div class="ui divider"></div> 
-                    <div class="two column row">
-                    </div>
+                </div>
               </div>
             </div>
         </div>
     </div>
 
-
+<!--<div class="ui grid centered">
+    <div class="eight wide column">
+        <div class="row column">
+            <div class="column">
+                <div class="ui form">
+                    <div class="fields">
+                        <div class="thirteen wide field">
+                            <textarea rows="2"></textarea>
+                        </div>
+                        <div class="two wide field">
+                            <button class="ui positive  button">Post</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row column">
+            <div class="ui green segment">
+                <div class="three column">
+                    <div class="left floated column">asdfasdfasd</div>
+                    <div class="right floated column">asdfasdfasd</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>-->
