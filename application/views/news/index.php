7b7527de5b56f76ff6c02946c11a5a256428a426
unknown <jeremiah.caballero.jc@gmail.com>
2016-06-08 17:02:00 +0800

    add animation

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -2,9 +2,10 @@
 <div>
 <a href="#" class="btn-float" id="options"><i class="inverted large plus icon"></i></a>
 </div>
-<i class="large plus icon" id="opt1"></i>
-<i class="large trash icon" id="opt2"></i>
-
+<div class="optn">
+<i class="large plus icon" ></i>
+<i class="large trash icon"></i>
+</div>
 
 <div id="news" class="nav_identifier">
 <div id="displaynews" ></div>
@@ -14,12 +15,14 @@ $(document).ready(function(){
     $.get('news/post_show',function(data){
         $('#displaynews').html(data);
     });
-    $("#opt1").hide();
-    $("#opt2").hide();
-    $("#opt3").hide();
-    $("#options").click(function(){
-        $("#opt1").transition('fly down',800,200);
+    $(".optn").hide();
 
+    $("#options").click(function(){
+        $(".optn").transition({
+                animation: 'fly down',
+                duration: 800,
+                interval: 200
+        });
     });
 });
 
