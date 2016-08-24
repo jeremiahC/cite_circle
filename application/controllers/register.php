5eb237d268ca81ca137a2c5f56bcc1af5480caf6
Jeremiah Caballero <jeremiah.caballero.jc@gmail.com>
2016-08-24 19:36:19 +0800

    added by jeremiah

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -29,22 +29,24 @@ class Register extends CI_Controller {
 		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[6]');
 		$this->form_validation->set_rules('password_confirm', 'Confirm Password', 'trim|required|min_length[6]|matches[password]');
 	
+
 		if ($this->form_validation->run() === false) {
 			// validation not ok, send validation errors to the view
-			$this->load->view('register_view');
-				
+			// $this->load->view('register_view');
+			echo json_encode(validation_errors());
+			
 		} else {
-				
 			// set variables from the form
 			$name = $this->input->post('name');
 			$email    = $this->input->post('email');
 			$password = $this->input->post('password');
-			
+
 			if ($this->aauth->create_user($email,$password,$name) == true) {
 	
-				$data['body'] = 'pages/register_success'; // cal your content
-				$this->load->view('template/template', $data);
-	
+				// $data['body'] = 'pages/register_success'; // cal your content
+				// $this->load->view('template/template', $data);
+				echo json_encode(validation_errors());
+
 			} else {
 				// send error to the view
 				$data['error'] = $this->aauth->print_errors();
@@ -52,9 +54,5 @@ class Register extends CI_Controller {
 			}
 				
 		}
-	
 	}
-	
-
-	
 }
